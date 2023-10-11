245fb8092fb0a98fdd66fc7facd1944b38d1c3d8
huycozy <huy@nevercode.io>
2023-10-11 10:27:11 +0700

    fix missing super on configureFlutterEngine
    Signed-off-by: Nguyen Huy <huy@nevercode.io>

--- android/app/src/main/kotlin/com/example/test_platformviews/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_platformviews/MainActivity.kt
@@ -5,6 +5,7 @@ import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
         flutterEngine
             .platformViewsController
             .registry
